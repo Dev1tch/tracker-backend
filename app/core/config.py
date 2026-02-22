ee20e1abb0d274f192102427aced97416b2ea667
5w7tch <nurch22@freeuni.edu.ge>
2026-02-23 01:47:29 +0400

    expiration increase

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     # Auth
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     
     model_config = SettingsConfigDict(
         env_file=".env",
